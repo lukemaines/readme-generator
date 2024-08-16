0fea75418152ee6d2cdc7df581bcda5c73fec827
l.maines <lmmaines28@gmail.com>
2024-08-16 19:50:45 -0400

    began the questions array in the index.js

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,24 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?",
+    },
+    {
+        type: "input", 
+        name: "description",
+        message: "Give a brief description of the project.",
+    }, 
+    {
+        type: "input",
+        name: "require",
+        message: "List your project dependencies",
+    },
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
